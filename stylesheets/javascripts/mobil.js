0d5b84ff85955abd297e5eefbbadb88f448a002e
Saraskov <43336948+Saraskov@users.noreply.github.com>
2018-12-20 15:07:23 +0100

    tester

--- stylesheets/javascripts/mobil.js
+++ stylesheets/javascripts/mobil.js
@@ -35,16 +35,3 @@ function myFunction (x) {
 var x = window.matchMedia("(max-width: 900px)")
 myFunction(x) // Call listener function at run time
 x.addListener(myFunction) // Attach listener function on state changes
-
-//Read more effect
-$(document).ready(function(){
-  $("#flip1").click(function(){
-    $("#show_me1").slideToggle("slow");
-  });
-});
-
-$(document).ready(function(){
-  $("#flip2").click(function(){
-    $("#show_me2").slideToggle("slow");
-  });
-});
---
+++ stylesheets/javascripts/read_more.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+//Read more effect
+$(document).ready(function(){
+  $("#flip1").click(function(){
+    $("#show_me1").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#flip2").click(function(){
+    $("#show_me2").slideToggle("slow");
+  });
+});
